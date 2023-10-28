1717856667a68d07e460edd23fb66877fc3091dc
Tristan W <tristanwang99@yahoo.com>
2023-10-27 23:23:12 -0700

    Updated services card visibility toggle etc

--- src/components/Service.js
+++ src/components/Service.js
@@ -6,18 +6,18 @@ const Service = ({ item }) => {
   return (
     <div key={item.id} className='flex flex-col items-center text-center'>
       <div id='service-container' className='mb-8 relative text-center'>
-        <img src={WhiteBackground} />
+        <img id='service-background' src={WhiteBackground} />
         <div className='absolute top-1/2 left-1/2 text-black flex flex-col justify-center items-center' style={{ transform: 'translate(-50%, -50%)' }}>
           <div id='service-front' className='flex flex-col justify-center items-center w-60 xl:w-80'>
             <div className='font-bold text-xl'>{item.name}</div>
             {item.duration ?
-              <div className='flex flex-row justify-center items-center w-fit bg-gray-700 px-2 py-1 mt-3'>
-                <FiClock className='text-white text-sm' />
-                <div className='text-sm ml-2 text-white'>{item.duration}</div>
+              <div className='flex flex-row justify-center items-center w-fit bg-gray-100 px-3 py-1 rounded-full mt-3'>
+                <FiClock className='text-gray-700 text-sm' />
+                <div className='text-sm ml-2 text-gray-700'>{item.duration}</div>
               </div> : <div></div>
             }
-            <div className='mt-3 text-gray-700'>{item.description}</div>
           </div>
+          <div id='service-back' className='mt-3 text-gray-700'>{item.description}</div>
         </div>
       </div>
     </div>
